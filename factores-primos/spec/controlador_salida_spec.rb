7b0dc64208a7604d736f125b383736df9d09aeb2
Gonza <g.d.escobar88@gmail.com>
2016-06-07 21:00:31 -0300

    controlador sa salida imprime en formato pretty con 360

--- factores-primos/spec/controlador_salida_spec.rb
+++ factores-primos/spec/controlador_salida_spec.rb
@@ -3,9 +3,14 @@ require_relative '../model/controlador_salida'
 
 describe 'controlador_salida' do
 
-  it 'deberia devolver un arreglo con 2 3 3 5 cuando descomponerNumerosPrimos 90' do
+  it 'deberia devolver Factores primos 90: 2 3 3 5  cuando formatearPretty 90' do
     formateador = ControladorDeSalida.new
   expect(formateador.formatearPretty(90,["2 3 3 5"])).to eq "Factores primos 90: 2 3 3 5 "
+  end
+
+  it 'deberia devolver Factores primos 360: 2 2 2 3 3 5 cuando formatearPretty 360' do
+    formateador = ControladorDeSalida.new
+  expect(formateador.formatearPretty(360,["2","2","2","3","3","5"])).to eq "Factores primos 360: 2 2 2 3 3 5 "
   end
 
 
