ba89d84f5560df534a8700c2fdb694a8e89830eb
Gonza <g.d.escobar88@gmail.com>
2016-06-07 21:22:12 -0300

    controlador sa salida imprime en formato quiet

--- factores-primos/model/controlador_salida.rb
+++ factores-primos/model/controlador_salida.rb
@@ -2,9 +2,17 @@ class ControladorDeSalida
 
   def formatearPretty(numero, numeroDescompuesto)
   	resultado = "" 
-  	numeroDescompuesto.each do |i|
-  	  resultado = resultado + "#{i}" + " "
+  	numeroDescompuesto.each do |numeroActual|
+  	  resultado += "#{numeroActual}" + " "
     end
     return "Factores primos " + numero.to_s + ": " + resultado
-  end 
+  end
+
+  def formatearQuiet (numeroDescompuesto)
+  	resultado = "" 
+  	numeroDescompuesto.each do |numeroActual|
+  	  resultado += "#{numeroActual}" + "\n"
+    end
+    return resultado
+  end  
 end
--- factores-primos/spec/controlador_salida_spec.rb
+++ factores-primos/spec/controlador_salida_spec.rb
@@ -5,7 +5,7 @@ describe 'controlador_salida' do
 
   it 'deberia devolver Factores primos 90: 2 3 3 5  cuando formatearPretty 90' do
     formateador = ControladorDeSalida.new
-  expect(formateador.formatearPretty(90,["2 3 3 5"])).to eq "Factores primos 90: 2 3 3 5 "
+  expect(formateador.formatearPretty(90,["2", "3", "3", "5"])).to eq "Factores primos 90: 2 3 3 5 "
   end
 
   it 'deberia devolver Factores primos 360: 2 2 2 3 3 5 cuando formatearPretty 360' do
@@ -16,6 +16,11 @@ describe 'controlador_salida' do
   it 'deberia devolver Factores primos 2: 2 cuando formatearPretty con numero que no se descompone' do
     formateador = ControladorDeSalida.new
   expect(formateador.formatearPretty(2,["2"])).to eq "Factores primos 2: 2 "
+  end
+
+  it 'deberia devolver 2\n3\n3\n5  cuando formatearQuiet 90' do
+    formateador = ControladorDeSalida.new
+  expect(formateador.formatearQuiet(["2","3","3","5"])).to eq "2\n3\n3\n5\n"
   end
 
 
