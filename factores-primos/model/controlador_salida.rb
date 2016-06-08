72c73bb59f56a57be5c6d2cfa14fb52f46935aea
Gonza <g.d.escobar88@gmail.com>
2016-06-07 22:09:21 -0300

    controlador sa salida imprime en formato pretty con orden

--- factores-primos/model/controlador_salida.rb
+++ factores-primos/model/controlador_salida.rb
@@ -1,7 +1,10 @@
 class ControladorDeSalida
 
-  def formatearPretty(numero, numeroDescompuesto)
+  def formatearPretty(numero, numeroDescompuesto, orden)
   	resultado = "" 
+  	if orden.eql?("des")
+  		numeroDescompuesto.reverse!
+  	end
   	numeroDescompuesto.each do |numeroActual|
   	  resultado += "#{numeroActual}" + " "
     end
@@ -10,9 +13,24 @@ class ControladorDeSalida
 
   def formatearQuiet (numeroDescompuesto)
   	resultado = "" 
+  	numeroDescompuesto.reverse!
   	numeroDescompuesto.each do |numeroActual|
   	  resultado += "#{numeroActual}" + "\n"
     end
     return resultado
   end  
+
+=begin
+  def formatear (numero, formato, numeroFactorizado, orden)
+  	resultado = "" 
+  	case formato.downcase
+  	  when "--format=pretty"
+  	  	resultado = fomatearPretty(numero, numeroFactorizado, orden)
+  	  when "--format=quiet"
+  	  	resultado = formatearQuiet(numeroFactorizado, orden)
+  	  else resultado = "Formato no aceptado. Las opciones posibles son: pretty o quiet."
+  	end
+    return resultado
+  end  
+=end
 end
--- factores-primos/spec/controlador_salida_spec.rb
+++ factores-primos/spec/controlador_salida_spec.rb
@@ -5,22 +5,27 @@ describe 'controlador_salida' do
 
   it 'deberia devolver Factores primos 90: 2 3 3 5  cuando formatearPretty 90' do
     formateador = ControladorDeSalida.new
-  expect(formateador.formatearPretty(90,["2", "3", "3", "5"])).to eq "Factores primos 90: 2 3 3 5 "
+  expect(formateador.formatearPretty(90,["2", "3", "3", "5"], "asc")).to eq "Factores primos 90: 2 3 3 5 "
+  end
+
+  it 'deberia devolver Factores primos 90: 5 3 3 2  cuando formatearPretty 90 des' do
+    formateador = ControladorDeSalida.new
+  expect(formateador.formatearPretty(90,["2", "3", "3", "5"], "des")).to eq "Factores primos 90: 5 3 3 2 "
   end
 
   it 'deberia devolver Factores primos 360: 2 2 2 3 3 5 cuando formatearPretty 360' do
     formateador = ControladorDeSalida.new
-  expect(formateador.formatearPretty(360,["2","2","2","3","3","5"])).to eq "Factores primos 360: 2 2 2 3 3 5 "
+  expect(formateador.formatearPretty(360,["2","2","2","3","3","5"], "asc")).to eq "Factores primos 360: 2 2 2 3 3 5 "
   end
 
   it 'deberia devolver Factores primos 2: 2 cuando formatearPretty con numero que no se descompone' do
     formateador = ControladorDeSalida.new
-  expect(formateador.formatearPretty(2,["2"])).to eq "Factores primos 2: 2 "
+  expect(formateador.formatearPretty(2,["2"], "asc")).to eq "Factores primos 2: 2 "
   end
 
-  it 'deberia devolver 2\n3\n3\n5  cuando formatearQuiet 90' do
+  it 'deberia devolver 5\n3\n3\n2  cuando formatearQuiet 90' do
     formateador = ControladorDeSalida.new
-  expect(formateador.formatearQuiet(["2","3","3","5"])).to eq "2\n3\n3\n5\n"
+  expect(formateador.formatearQuiet(["2","3","3","5"])).to eq "5\n3\n3\n2\n"
   end
 
 
